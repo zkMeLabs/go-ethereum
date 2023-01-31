734fad127acd3f703d405fad87cd1bbaf51980d7
Freddy Caceres <facs95@gmail.com>
2023-01-31 18:22:58 -0500

    fix conflicts

--- core/vm/interpreter.go
+++ core/vm/interpreter.go
@@ -44,7 +44,6 @@ type ScopeContext struct {
 	Contract *Contract
 }
 
-<<<<<<< HEAD
 // keccakState wraps sha3.state. In addition to the usual hash methods, it also supports
 // Read to get a variable amount of data from the hash state. Read is faster than Sum
 // because it doesn't copy the internal state, but also modifies the internal state.
@@ -52,9 +51,8 @@ type keccakState interface {
 	hash.Hash
 	Read([]byte) (int, error)
 }
-=======
+
 var _ Interpreter = &EVMInterpreter{}
->>>>>>> 7cd2d5a4d (imp(vm): Interpreter interface (#2))
 
 // EVMInterpreter represents an EVM interpreter
 type EVMInterpreter struct {
