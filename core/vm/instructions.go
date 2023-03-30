e1e875757bd878ad95950e352075bb9fc7973ccf
Vladislav Varadinov <vladislav.varadinov@gmail.com>
2023-03-30 11:58:08 +0300

    reverse

--- core/vm/instructions.go
+++ core/vm/instructions.go
@@ -17,8 +17,6 @@
 package vm
 
 import (
-	"encoding/hex"
-	"fmt"
 	"sync/atomic"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -807,9 +805,6 @@ func opReturn(pc *uint64, interpreter *EVMInterpreter, scope *ScopeContext) ([]b
 func opRevert(pc *uint64, interpreter *EVMInterpreter, scope *ScopeContext) ([]byte, error) {
 	offset, size := scope.Stack.Pop(), scope.Stack.Pop()
 	ret := scope.Memory.GetPtr(int64(offset.Uint64()), int64(size.Uint64()))
-
-	reason := hex.EncodeToString(scope.Stack.Back(0).Bytes())
-	fmt.Printf("REVERT opcode received with reason: %s\n", reason)
 
 	interpreter.returnData = ret
 	return ret, ErrExecutionReverted
